e23117ec104313b085cdd8fd7c3a0076745e4519
Jerowe Tan <jerowe.tan99@gmail.com>
2024-03-01 11:15:12 +0800

    Fixed bugs on frontend gatekeeper

--- Frontend/src/Gatekeeper.jsx
+++ Frontend/src/Gatekeeper.jsx
@@ -1,5 +1,5 @@
 import { Suspense, useCallback, useEffect, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, redirect, useNavigate } from "react-router-dom";
 import PageLoader from "./Utilities/PageLoader";
 import { ApiVerify } from "./Helper/Api";
 import PageError from "./Utilities/PageError";
@@ -9,16 +9,15 @@ const gatepolice = {
     needAuthentication: (helper)=>{
         const token = localStorage.getItem('token');
         if(!token){
-            helper.navigation('/login');
+            redirect('/login');
             return false;
         }
         return true;
     },
     noAuthentication: (helper)=>{
         const token = localStorage.getItem('token');
-        console.log(token);
         if(token){
-            helper.navigation('/');
+            redirect('/');
             return false;
         }
         return true;
@@ -29,30 +28,24 @@ export default (option)=>{
     const Content = option.children;
     const Rule = option.type;
     const [c_verifying, s_verifying] = useState(true);
-    const [c_error, s_error] = useState(true);
 
+    //This will be use inside a rule component
     const helper = {
         navigation: useNavigate(),
     }
+
+    //This will be the one who manage the rotation of rules
     useEffect(()=>{
         if( typeof Rule === "array"){
-            if(Rule.every(x=> gatepolice[x](helper))){
-                s_error(false);
-            }
+            Rule.every(x=> gatepolice[x](helper))
         }else{
-            if(gatepolice[Rule](helper)){
-                s_error(false);
-            }
+            gatepolice[Rule](helper)
         }
             
         s_verifying(false);
-    }, [s_error, s_verifying]);
+    }, [s_verifying]);
 
     return <>
-        { c_verifying ? <PageLoader /> : (
-            c_error ? <PageError /> : <>
-                {Content}
-            </>
-        ) }
+        { c_verifying ? <PageLoader /> : Content }
     </>
 }
